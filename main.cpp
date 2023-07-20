0149460fe521f1caa38ada733cbd2dc6fcaa48a5
Reed0610 <shanereed19@gmail.com>
2023-07-20 09:55:30 +0000

    Commit 3
    Signed-off-by: Reed0610 <shanereed19@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,21 +3,32 @@
 
 #define PI 3.141592654
 
-static DevI2C devI2c(PB_11,PB_10);
-static LSM6DSLSensor acc_gyro(&devI2c,0xD4,D4,D5); // high address
+static DevI2C devI2c(PB_11, PB_10);
+static LSM6DSLSensor acc_gyro(&devI2c, 0xD4, D4, D5); // high address
 
+float computeAngle(int x, int y, int z) {
+    // Calculate the pitch (or slope) angle using accelerometer readings
+    // Take the acceleration values in X, Y, and Z axes as inputs
 
-float computeAngle(int x, int y, int z){
-    float res = 0;
+    // Convert the acceleration values to floating-point numbers
+    float accX = static_cast<float>(x);
+    float accY = static_cast<float>(y);
+    float accZ = static_cast<float>(z);
 
-    return res;
+    // Calculate the pitch angle using Equation 6
+    float pitch = atan2(accY, sqrt(accX * accX + accZ * accZ));
+
+    // Convert the angle from radians to degrees
+    float pitchDegrees = pitch * 180.0f / PI;
+
+    return pitchDegrees;
 }
 
-/* Simple main function */
 int main() {
     uint8_t id;
     int32_t axes[3];
-    float res=0;
+    float res = 0;
+
     acc_gyro.init(NULL);
 
     acc_gyro.enable_x();
@@ -27,14 +38,17 @@ int main() {
     acc_gyro.read_id(&id);
     printf("LSM6DSL accelerometer & gyroscope = 0x%X\r\n", id);
 
-    while(1) {
-
+    while (1) {
+        // Read the accelerometer axes
         acc_gyro.get_x_axes(axes);
+
+        // Compute the angle using the computeAngle function
         res = computeAngle(axes[0], axes[1], axes[2]);
+
+        // Print the accelerometer values and the computed angle
         printf("LSM6DSL: %6d, %6d, %6d, %3.2f\r\n", axes[0], axes[1], axes[2], res);
 
-
-        thread_sleep_for(2000);
-
+        // Wait for 2 seconds
+        ThisThread::sleep_for(2000ms);
     }
 }
